5dc277b765f581291bb03e6b2a7cf00d997c1284
Maurizio von Flotow <mvonflotow@gmail.com>
2013-10-24 22:50:35 -0700

    Changed delay (sleep) to 0.5 seconds Added a comment section of each axis added "import pygame" (necessary?) Added "pygame.event.get()" and ".pump()" to while loop (because the internet told me to)

--- Python/joystick_driver.py
+++ Python/joystick_driver.py
@@ -1,9 +1,10 @@
 __author__ = 'Adrien'
-
 from pygame import joystick
+import pygame
 import time
 
 joystick.init()
+pygame.display.init()
 
 print 'joystick count: ', joystick.get_count()
 
@@ -11,10 +12,23 @@ joy = joystick.Joystick(0)
 joy.init()
 
 print 'num axes: ', joy.get_numaxes()
+"""
+Axis 0: (Left,Right) = (-32768, 32767), center = -1
+Axis 1: (Forward,Backward) = (-32768, 32767), center = -1
+Axis 2: Left throttle (Forward, Backward) = (-32768, 32767), center = -1
+Axis 3: Twist (CCW, CW) = (-32768, 32767), center = -1
+Axis 4: Right Throttle (Forward, Backward) = (-32768, 32767), center = -1
+"""
 print 'num buttons: ', joy.get_numbuttons()
 
 while True:
-    print joy.get_axis(0), joy.get_axis(1), joy.get_axis(2), joy.get_axis(3), joy.get_axis(4)
-    time.sleep(0.1)
+    pygame.event.get()
+    pygame.event.pump()
+    print joy.get_axis(0)
+    print "Left/Right Axis:", joy.get_axis(0)
+    print "Forw/Backw Axis:", joy.get_axis(1)
+
+    time.sleep(0.5)
+
 
 
