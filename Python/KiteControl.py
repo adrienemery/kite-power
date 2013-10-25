16317588e576f61d2758dc82287ae261f3b06146
Adrien Emery <adrien.emery@gmail.com>
2013-10-25 12:59:36 -0700

    Tested and working.

--- Python/KiteControl.py
+++ Python/KiteControl.py
@@ -1,61 +1,72 @@
-__author__ = 'Adrien'
 from pygame import joystick
 import pygame
 import time
 import serial
 
 
-def write_to_serial(msg):
-    if ser.isOpen():
-        if not '/' in msg:
-            msg += '/'
-        ser.write(msg)  # Terminating char '/' must always be appended to the end of the msg
-        return True
-    else:
-        print 'Serial not available: ' + msg
-        return False
+class KiteControl(object):
+
+    def __init__(self):
+        self.port = 'COM4'
+        self.baud = 9600
+
+        try:
+            self.ser = serial.Serial(self.port, self.baud)
+            print "Successfully opened Serial"
+        except serial.SerialException:
+            self.ser = None
+            print "Could not open Serial!"
+
+        # Initialize Joystick object
+        joystick.init()
+        pygame.display.init()
+        self.joy = joystick.Joystick(0)
+        self.joy.init()
+
+        self.x_axis = 0  # this is usually 0
+        self.throttle_axis = 1  # change this to the correct axis for the joystick
+
+        self.last_turn_val = 0
+        self.last_sheet_val = 0
+
+    def write_to_serial(self, msg):
+        if self.ser:
+            if not '/' in msg:
+                msg += '/'
+            self.ser.write(msg)  # Terminating char '/' must always be appended to the end of the msg
+            return True
+        else:
+            print 'Serial not available: ' + msg
+            return False
+
+    def turn(self, val):
+        val *= 30
+        val = str(int(val))  # convert to string
+
+        if not val == self.last_turn_val:
+            self.last_turn_val = val
+            return self.write_to_serial('t' + val)
+
+    def sheet(self, val):
+        val = (val + 1)/2 * 96  # maps the values (-1,1) to (0,96) This might need to be reversed?
+        val = str(int(val))
+
+        if not val == self.last_sheet_val:
+            self.last_sheet_val = val
+            return self.write_to_serial('p' + val)
+
+    def run(self):
+
+        while True:
+            pygame.event.get()
+            pygame.event.pump()
+
+            self.turn(self.joy.get_axis(self.x_axis))
+            self.sheet(self.joy.get_axis(self.throttle_axis))
+
+            time.sleep(0.05)
 
 
-def turn(val):
-    val *= 30
-    val = str(val)  # convert to string
-    return write_to_serial('t' + val)
-
-
-def sheet(val):
-    val = (val + 1)/2 * 96  # maps the values (-1,1) to (0,96) This might need to be reversed?
-    val = str(val)
-    return write_to_serial('p' + val)
-
-
-port = 'COM4'
-baud = 9600
-
-try:
-    ser = serial.Serial(port, baud)
-    print "Successfully opened Serial"
-except serial.SerialException:
-    print "Could not open Serial!"
-
-# Initialize Joystick object
-joystick.init()
-pygame.display.init()
-joy = joystick.Joystick(0)
-joy.init()
-
-x_axis = 0
-throttle_axis = 4
-
-while True:
-    pygame.event.get()
-    pygame.event.pump()
-
-    turn(joy.get_axis(x_axis))
-    sheet(joy.get_axis(throttle_axis))
-
-    time.sleep(0.2)
-
-
-
-
+control = KiteControl()
+control.run()
 
